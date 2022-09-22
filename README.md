eb110b201c8af209cb4df4bb545cc1edd9bcacd2
Mourad-ouerghi <mouraouerghi12@gmail.com>
2022-09-22 18:38:15 +0100

    running script.sh from 'create' command and added README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Project creation automation
+The purpsoe of this project is to automate the repetitive task of initializing git in a local project then linking it to github using shell script and python.
+### Pre-setup :
+* go to your terminal and install selenium for python :
+
+    `pip install selenium`
+### Setup : 
+    1.In the file script.sh line 6 change the path to where you want your projects to be created (accordingly to the line 12)
+    2. In the file script.sh line 10 change the path of the script.py file
+    3. if you're using chrome put the lines 12->15 in a comment
+    4. open powershell and paste this command so you can run it from anywhere :
+    Set-Alias -Name create -Value path-of-script.sh 
+### Usage :
+* `create` name-of-project <br>
+and let the magic happens
--- script.sh
+++ script.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-read -p "Enter the name of the project : " projectName
-read -p "Enter your github username : " github_username
-read -s -p "Enter your github password : " github_password
-cd "D:/personal/self-dev/projects"
-mkdir ${projectName}
-cd ${projectName}
-git init
-cd "D:/personal/self-dev/scriptShell/projects/project-automation"
-python script.py ${projectName} ${github_username} ${github_password}
-cd "D:/personal/self-dev/projects/${projectName}"
-touch "README.md"
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Mourad-Ouerghi/${projectName}.git
-git push -u origin main
+    read -p "Enter the name of the project : " projectName
+    read -p "Enter your github username : " github_username
+    read -s -p "Enter your github password : " github_password
+    cd "D:/personal/self-dev/projects"
+    mkdir ${projectName}
+    cd ${projectName}
+    git init
+    cd "D:/personal/self-dev/scriptShell/projects/project-automation"
+    python script.py ${projectName} ${github_username} ${github_password}
+    cd "D:/personal/self-dev/projects/${projectName}"
+    touch "README.md"
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/Mourad-Ouerghi/${projectName}.git
+    git push -u origin main
+
